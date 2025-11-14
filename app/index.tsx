7915ffaaf5f6fb6e76bcbf348717967cf1f60330
Andrew Arreola <andarreola@csumb.edu>
2025-11-14 00:34:23 -0800

    Connect with firebase auth Made layout work with firebase. Users will be stored now and accounts can be created.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import {Redirect} from 'expo-router';
+import {useEffect, useState} from 'react';
+
 export default function IndexPage(){
     // Redirect to login screen for now (no auth yet)
     return <Redirect href = "/login"/>;
