ac82bf748a9d7ba1768a30e1659a3bfadd8462b8
Andrew Arreola <andarreola@csumb.edu>
2025-11-14 16:39:46 -0800

    Create tab bars Create tab bars to go between pages.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,10 @@ export default function TabsLayout(){
     return (
         <Tabs screenOptions = {{headerShown: false}}>
             <Tabs.Screen name = "home"/>
+            <Tabs.Screen name = "workouts"/>
+            <Tabs.Screen name = "createPost"/>
+            <Tabs.Screen name = "progress"/>
+            <Tabs.Screen name = "profile"/>
         </Tabs>
     )
 }
